f3b4d10f38eae9b470b5b60fa978e6bbcd9e76c0
Diar Muradi <theahtic@gmail.com>
2025-09-05 23:09:47 +0300

    feat: exercise-8

---
+++ Personal/exercise-8.cpp
@@ -0,0 +1,17 @@
+// Ask the user to enter a number // Read the number from user input // Calculate the square of the number // Print the result to the user
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Enter a number: ";
+
+    int num;
+
+    cin >> num;
+
+    cout << "The square of the number is: " << num * num << endl;
+
+    return 0;
+}
