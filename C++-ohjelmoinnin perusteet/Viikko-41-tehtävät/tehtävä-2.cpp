a1e7e0574d41f0a2f7fbf3fa8e96c2c30f1eb61d
Riyadh Muradi <theahtic@gmail.com>
2025-10-23 19:55:20 +0300

    feat: corrected assigment

--- C++-ohjelmoinnin perusteet/Viikko-41-tehtävät/tehtävä-2.cpp
+++ C++-ohjelmoinnin perusteet/Viikko-41-tehtävät/tehtävä-2.cpp
@@ -20,28 +20,41 @@ Tulostaa taulukon sisällön sekä piirtää jokaiselle arvolla tähtipalkin (hi
     Arvo 8: ********
 */
 
+^
+/* Tosi huono esimerkki */
+
 int main()
 {
     setlocale(LC_ALL, "fi_FI.UTF-8");
 
     const int LUKUJEN_MAARA = 10;
     int luvut[LUKUJEN_MAARA];
+    int maara[21] = {0};
 
     cout << "Kirjoita " << LUKUJEN_MAARA << " lukua väliltä 1-20: ";
 
     for (int i = 0; i < LUKUJEN_MAARA; i++)
     {
         cin >> luvut[i];
+        if (luvut[i] >= 1 && luvut[i] <= 20)
+        {
+            maara[luvut[i]]++;
+        }
     }
 
-    for (int i = 0; i < LUKUJEN_MAARA; i++)
+    cout << "\nHistogrammi";
+
+    for (int i = 1; i <= 20; i++)
     {
-        cout << "Arvo " << luvut[i] << ": ";
-        for (int j = 0; j < luvut[i]; j++)
+        if (maara[i] > 0)
         {
-            cout << "*";
+            cout << i << ": ";
+            for (int j = 0; j < maara[i]; j++)
+            {
+                cout << "*";
+            }
+            cout << endl;
         }
-        cout << endl;
     }
 
     return 0;
